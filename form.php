34c9ae96c34d86cf187d9e1ca303b95816b43b0e
wintercricket <hodhodhod2003@gmail.com>
2021-08-22 22:13:43 -0500

    create files for php form processing

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="form.css">
+	<title>HTML Form</title>
+</head>
+<body>
+	<h1>PHP Form Processing</h1>
+<div>
+	<form method="POST" action="form-response.php" >
+		
+
+
+	</form>
+		Name: <input type="text" name="name">
+		<br>
+		Password: <input type="password" name="password">
+</div>
+</body>
+</html>
